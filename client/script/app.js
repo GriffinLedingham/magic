cff224b1d46a63bd9b2952ff204100dd8ab24516
Griffin Ledingham <gcledingham@gmail.com>
2016-01-25 12:10:39 -0800

    Script sock io address updated

--- client/script/app.js
+++ client/script/app.js
@@ -11,7 +11,7 @@ var priority_user_id;
 var socket;
 
 function init() {
-    socket = io.connect('http://localhost:3000');
+    socket = io.connect('http://mtgfish.herokuapp.com/');
 
     setUpBindings();
     initTemplates();
